b9ee6ce994084f81964e8c740646861322a715cd
Howard Chang <howard3433@gmail.com>
2015-04-24 17:43:46 +0800

    fix tag index issue

--- _plugins/tag_gen.rb
+++ _plugins/tag_gen.rb
@@ -10,6 +10,7 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
+      self.data['category'] = "Tag Index"
       self.data['title'] = "Posts Tagged &#60;"+tag+"&#62;"
     end
   end
